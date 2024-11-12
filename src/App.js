1a8e42abd573bddbe0b8d0a1c61df73bcd0531fb
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-11-12 10:37:55 +0100

    set-up and building the layout

---
+++ src/App.js
@@ -0,0 +1,50 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1> ⛩ Wander List 🏯 </h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return <li></li>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      {" "}
+      🧳 You have X items on your list, and you already pached X (X%)
+    </footer>
+  );
+}
